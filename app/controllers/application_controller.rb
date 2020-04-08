b9a188464e45a354be1362e7eda064f6ebbd56f3
Patrick Oliver <poliver24@hotmail.com>
2020-04-08 11:24:56 +0100

    set_time_zone method added to application controller in order to match time zone to London

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
       @current_user = nil
     end
   end
+
+  around_action :set_time_zone
+
+  def set_time_zone(&block)
+    time_zone = current_user.try(:time_zone) || 'London'
+    Time.use_zone(time_zone, &block)
+  end
 end
