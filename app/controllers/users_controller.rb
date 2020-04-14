a40f07ec9fa1d9c4ddebd95c12d38ef03555363e
Patrick Oliver <poliver24@hotmail.com>
2020-04-14 13:24:11 +0100

    404 page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
 
   def show
+    return render_not_found unless User.exists?(params[:id])
     @user = User.find(params[:id])
     @posts = Post.where(user_id: @user.id).order(created_at: :desc)
   end
+  
+  # def show
+  #   if User.find_by_id(params[:id])
+  #     @user = User.find_by_id(params[:id]) # returns nil or a record
+  #     @posts = Post.where(user_id: @user.id).order(created_at: :desc)
+  #   else
+  #     p 'i dont exist'
+  #     flash[:error] = "#{params[:id]} is not a User."
+  #     render_not_found
+  #   end
+  # end
 
   def index
     if current_user
