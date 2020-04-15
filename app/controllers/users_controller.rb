51e0cf9ca0b3f638234a85fce00cd1e5f412a0ca
dariathompson <daria.kambalina@gmail.com>
2020-04-15 10:12:25 +0100

    only signed in user can see other users walls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,20 @@
 class UsersController < ApplicationController
 
   def show
-    return render_not_found unless User.exists?(params[:id])
-    @user = User.find(params[:id])
-    @posts = Post.where(user_id: @user.id).order(created_at: :desc)
+    if current_user
+      return render_not_found unless User.exists?(params[:id])
+      @user = User.find(params[:id])
+      @posts = Post.where(user_id: @user.id).order(created_at: :desc)
+    else
+      redirect_to new_user_session_path
+    end
   end
-  
-  # def show
-  #   if User.find_by_id(params[:id])
-  #     @user = User.find_by_id(params[:id]) # returns nil or a record
-  #     @posts = Post.where(user_id: @user.id).order(created_at: :desc)
-  #   else
-  #     p 'i dont exist'
-  #     flash[:error] = "#{params[:id]} is not a User."
-  #     render_not_found
-  #   end
-  # end
 
   def index
     if current_user
       @posts = Post.where(user_id: current_user.id).order(created_at: :desc)
     else
-      redirect_to login_url
+      redirect_to new_user_session_path
     end
   end
 
