e025217a61d3b55668dd7b822e0bdb1139c8a203
Luke Donovan <lukelamnisos@icloud.com>
2020-04-17 11:33:42 +0100

    added edit button for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,24 @@ def create
   redirect_to user_path(:id => @post.user.id)
 end
 
+def edit
+  p params
+  # @post = Post.find(params[:id])
+  @comment = Comment.find(params[:id])
+  if !owner?(@comment.user_id) || Time.now > (@comment.created_at + (600))
+    redirect_to user_path(:id => @comment.post_id), notice: "You cannot edit this post"
+  end
+end
+
+def update
+  @comment = Comment.find(params[:id])
+  if @comment.update_attribute(:body, comment_params['body'])
+    redirect_to user_path(:id => @comment.post.wall_id)
+  else
+    render 'edit'
+  end
+end
+
 private
   def comment_params  
     params.require(:comment).permit(:post_id, :body)
